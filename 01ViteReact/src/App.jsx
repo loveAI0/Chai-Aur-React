fcc6a6702743626981c5023ef9a0a13321a8ca5c
amrit <rohitraut095@gmail.com>
2025-05-28 02:03:36 +0530

    upto 3 vidoes lessons

---
+++ 01ViteReact/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Chai from "./test";
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Chai/>
+    </div>
+    
+
+  )
+}
+
+export default App
---
+++ 01ViteReact/src/test.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h1>Chai is ready on the beach</h1>
+    )
+}
+export default Chai;
